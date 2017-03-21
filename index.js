6640e8b6cca3d8b6daeb20129ce97af0acf5795c
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-03-21 23:37:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Vadym Yatsyuk on 21/03/2017
+ */
+
+let app = require('express')();
+let resposeTime = require('response-time');
+let axios = require('axios');
+let redis = require('redis');
+
+let redisClient = redis.createClient();
+
+redisClient.on('error', err => {
+  console.log('Redis Error: ' + err);
+});
+
+app.set('port', 8000);
+
+app.use(resposeTime());
+
+app.get('/api/:username', (req, res) => {
+  const username = req.params.username;
+
+  redisClient.get(username, (error, totalStars) => {
+    if (!error && totalStars) {
+      sendAnswer(res, totalStars);
+      return;
+    }
+
+    axios.get(`https://api.github.com/users/${ username }/repos?per_page=100`)
+      .then(repositories => {
+        return repositories.data.reduce((prev, curr) => {
+          return prev + curr.stargazers_count;
+        }, 0);
+      })
+      .then(totalStars => {
+        redisClient.setex(username, 60, totalStars);
+        sendAnswer(res, totalStars);
+      })
+      .catch(response => {
+        res.send(response);
+      });
+  });
+});
+
+function sendAnswer(res, totalStars) {
+  res.json({
+    totalStars
+  }).end();
+}
+
+app.listen(app.get('port'), () => {
+  console.log('Server running on port: ', app.get('port'));
+});
